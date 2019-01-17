c8a47dea78b0f365594f83bf42b784f0eb8f16ef
sjena <sjena@yapstone.com>
2019-01-17 12:19:51 +0530

    reacr routing

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,28 +1,69 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import { Navbar , NavbarBrand, Button,Jumbotron}  from "reactstrap";
+import { Navbar, NavbarBrand, Button, Jumbotron, Nav, NavbarToggler, Collapse, NavItem } from "reactstrap";
+import { NavLink } from "react-router-dom";
+class Header extends Component {
+  constructor(props){
+     super(props);
+     this.state = {
+         isNavOpen:false
+     }
+     this.toggleNav = this.toggleNav.bind(this);
+  }
 
-class Header extends Component{
-
-
-    render(){
-        return(
+  toggleNav(){
+     this.setState({
+         isNavOpen : !this.state.isNavOpen
+     });
+  }
+    render() {
+        return (
             <>
-            <Navbar dark > 
-     <div className= "container">
-       <NavbarBrand href="/"> Confusion  Application </NavbarBrand>
-     </div>
-     </Navbar>
-     <Jumbotron>
-    <div className = "container">
-       <div className = "row row-header">
-           <div className = "col-12 col-sm-6">
-            <h1>Rustorante Con Fusion</h1>
-            <p>Since the hotel industry has been operational for centuries, various hotelier quotes exist that can help to explain how the industry has changed over the many years.</p>
-           </div>
-       </div>
-    </div>
-    </Jumbotron>       
+                <Navbar dark color="primary" expand="md">
+                    <div className="container">
+                    <NavbarToggler onClick={this.toggleNav}/>
+                        <NavbarBrand className="mr-auro" href='/'>
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Restorent Con Fusion"></img>
+                        </NavbarBrand>
+                        <Collapse isOpen = {this.state.isNavOpen} navbar> 
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/home">
+                                    <span className="fa fa-home fa-lg"></span>Home
+                                </NavLink>
+                            </NavItem>
+
+                             <NavItem>
+                                <NavLink className="nav-link" to="/aboutus">
+                                    <span className="fa fa-info fa-lg"></span> About Us
+                                </NavLink>
+                            </NavItem>
+
+                             <NavItem>
+                                <NavLink className="nav-link" to="/menu">
+                                    <span className="fa fa-list fa-lg"></span> Menu
+                                </NavLink>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavLink className="nav-link" to="/contactus">
+                                    <span className="fa fa-address-card fa-lg"></span> Contact Us
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Rustorante Con Fusion</h1>
+                                <p>Since the hotel industry has been operational for centuries, various hotelier quotes exist that can help to explain how the industry has changed over the many years.</p>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
             </>
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
-import { Navbar , NavbarBrand, Button}  from "reactstrap";
+import { Navbar, NavbarBrand, Button } from "reactstrap";
 import Menu from "./MenuComponent";
 import Dish from "./DishDetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
-  constructor(props){
-     super(props);
+  constructor(props) {
+    super(props);
 
-     this.state = {
-       dishes: DISHES,
-       selectedDish: null
-     }
+    this.state = {
+      dishes: DISHES,
+
+    }
   }
 
-  onDishSelect(dishId){
-    this.setState({ selectedDish: dishId});
-}
-
   render() {
+    const Homepage = () => {
+      return(
+        <Home/>
+      );
+    }
     return (
       <div>
-        <Header/>
-     <Menu dishes = {this.state.dishes} onClick = {(dishId) => this.onDishSelect(dishId)}/>
-     <Dish dish= {this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]}/>
-     <Footer/>
+        <Header />
+        <Switch>
+           <Route path ="/home" component = {Homepage}/>
+           <Route exact path = "/menu" component = {()=><Menu dishes = {this.state.dishes} />}/>
+           <Route/>
+           <Redirect to ="home"/>
+        </Switch>
+        <Footer />
       </div>
     );
   }
 }
 
 export default Main;
- 
